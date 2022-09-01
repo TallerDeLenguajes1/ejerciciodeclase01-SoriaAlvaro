b52dc8356ca58c2c08b6d1db7d75e8caececce9e
SoriaAlvaro <alvarosoria.universidad@gmail.com>
2022-09-01 20:52:52 -0300

    problema 2, error controlado

--- Problema02/Program.cs
+++ Problema02/Program.cs
@@ -11,8 +11,14 @@ namespace Problema02
 
             num1 = 30;
             num2 = 60;
-            result = Convert.ToByte(num1 * num2);
-            Console.WriteLine("{0} x {1} = {2}", num1, num2, result);
+            try{
+                result = Convert.ToByte(num1 * num2);
+                Console.WriteLine("{0} x {1} = {2}", num1, num2, result);
+            }catch(OverflowException ex){
+                System.Console.WriteLine("\nEl valor era demasiado grande o demasiado pequeño para el tipo byte.\n   El tipo byte representa un entero sin signo de 0 a 255.");
+            }catch(Exception ex){
+                System.Console.WriteLine(ex.Message);
+            }
             Console.ReadLine();
         }
     }
