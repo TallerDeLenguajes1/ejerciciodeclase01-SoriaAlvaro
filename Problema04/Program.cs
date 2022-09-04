91ca9fb420f353750027650936154ea48dec5159
SoriaAlvaro <alvarosoria.universidad@gmail.com>
2022-09-04 20:38:25 -0300

    error controlado

--- Problema04/Program.cs
+++ Problema04/Program.cs
@@ -8,7 +8,10 @@ namespace Problema04
         {
             int a = 1;
             int b = 2;
-            Console.WriteLine("{0} - {1} - {2}", a, b);
+            try{Console.WriteLine("{0} - {1} - {2}", a, b);}
+            catch(FormatException ex){
+                System.Console.WriteLine("El índice (basado en cero) debe ser mayor o igual a cero y menor que el tamaño de la lista de argumentos.");
+            }
             Console.ReadLine();
         }
     }
